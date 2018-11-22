c0b8ef9d224b7da856666b18c3d12f877feef826
Jeremy.Lee <Jeremy.Lee@JeremyLeedeMBP.wv.cc.cmu.edu>
2018-11-22 18:43:10 -0500

    two sum
    two sum

---
+++ 01_two_sum.py
@@ -0,0 +1,20 @@
+class Solution:
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        adic = {}
+        index = 0
+        for num in nums:
+            adic[num] = index
+            index = index + 1
+        for i in range(target+1):
+            try:
+                return [adic[i],adic[target-i]]
+            except:
+                pass
+
+
+print(Solution().twoSum([1,2,3],5))
