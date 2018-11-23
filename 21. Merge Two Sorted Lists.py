450802159a652a735ed02bafc3973301a7f3cffc
Jingwei4CMU <jingwei4@andrew.cmu.edu>
2018-11-23 13:33:14 -0500

    21. Merge Two Sorted Lists
    Runtime: 48 ms, faster than 69.90% of Python3 online submissions for Merge Two Sorted Lists.

---
+++ 21. Merge Two Sorted Lists.py
@@ -0,0 +1,66 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution:
+    def mergeTwoLists(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        # Runtime: 48 ms, faster than 69.90% of Python3 online submissions for Merge Two Sorted Lists.
+        l3 = None
+        flag1 = l1
+        flag2 = l2
+        while flag1 and flag2:
+            if flag1.val <= flag2.val:
+                if l3:
+                    flag3.next = ListNode(flag1.val)
+                    flag3 = flag3.next
+                    flag1 = flag1.next
+                else:
+                    l3 = ListNode(flag1.val)
+                    flag1 = flag1.next
+                    flag3 = l3
+            else:
+                if l3:
+                    flag3.next = ListNode(flag2.val)
+                    flag3 = flag3.next
+                    flag2 = flag2.next
+                else:
+                    l3 = ListNode(flag2.val)
+                    flag2 = flag2.next
+                    flag3 = l3
+        while flag1:
+            if l3:
+                flag3.next = ListNode(flag1.val)
+                flag3 = flag3.next
+                flag1 = flag1.next
+            else:
+                l3 = ListNode(flag1.val)
+                flag1 = flag1.next
+                flag3 = l3
+        while flag2:
+            if l3:
+                flag3.next = ListNode(flag2.val)
+                flag3 = flag3.next
+                flag2 = flag2.next
+            else:
+                l3 = ListNode(flag2.val)
+                flag2 = flag2.next
+                flag3 = l3
+
+
+        return l3
+
+l1 = ListNode(1)
+l1.next = ListNode(2)
+l1.next.next = ListNode(4)
+l2 = ListNode(1)
+l2.next = ListNode(3)
+l2.next.next = ListNode(4)
+
+print(Solution().mergeTwoLists(l1,l2))
